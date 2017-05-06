3f1d6a9261710adc85b698ecad7529bcf693bec6
bugogish <bugogish@gmail.com>
2017-05-06 20:28:27 +0300

    fight logic slightly improved

--- src/ru/spbau/mit/core/Player.java
+++ src/ru/spbau/mit/core/Player.java
@@ -7,18 +7,25 @@ import ru.spbau.mit.core.items.Shield;
 import java.util.ArrayList;
 
 public class Player extends Character {
+    private static final char DEFAULT_ICON = 'Δ';
     private static final Stats baseStats = new Stats(100, 100, 1);;
     private Inventory mInventory = new Inventory();
+    private static final double ARMOR_POWER_DECREASE = 0.2;
+    private ArrayList<Item> equippedItems = new ArrayList<>();
     private Item weaponEquipped;
     private Item defenceEquipped;
     private static final double FIGHT_COEFFICIENT = 0.01;
 
     public Player() {
-        super('Δ', baseStats);
+        super(DEFAULT_ICON, baseStats);
     }
 
     public void pickUp(Item item) {
         mInventory.add(item);
+    }
+
+    public Inventory getInventory() {
+        return mInventory;
     }
 
     public void equipItem(Item item) {
@@ -30,12 +37,8 @@ public class Player extends Character {
         }
     }
 
-    public Inventory getmInventory() {
-        return mInventory;
-    }
-
     @Override
     public int getFightPower() {
-        return (int) Math.round(getStats().getStamina() * FIGHT_COEFFICIENT);
+        return Math.max(0, (int) Math.round(getStats().getStamina() - getStats().getArmor() * ARMOR_POWER_DECREASE));
     }
 }
--- src/ru/spbau/mit/core/mobs/Monster.java
+++ src/ru/spbau/mit/core/mobs/Monster.java
@@ -5,7 +5,7 @@ import ru.spbau.mit.core.Stats;
 public class Monster extends Mob {
     private static final char DEFAULT_ICON = '⏧';
     private static final Stats DEFAULT_STATS = new Stats(20, 10, 2);
-    private static final double FIGHT_COEFFICIENT = 1e-2;
+    private static final double ARMOR_POWER_DECREASE = 0.4;
 
     public Monster() {
         super(DEFAULT_ICON, DEFAULT_STATS);
@@ -13,6 +13,6 @@ public class Monster extends Mob {
 
     @Override
     public int getFightPower() {
-        return (int) Math.round(getStats().getStamina() * FIGHT_COEFFICIENT);
+        return Math.max(0, (int) Math.round(getStats().getStamina() - getStats().getArmor() * ARMOR_POWER_DECREASE));
     }
 }
